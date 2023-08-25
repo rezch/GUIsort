d398f2d06e9d4cedd1269accd02c4fd2053a9b0c
rezch <dany1008@yandex.ru>
2023-08-25 16:49:26 +0300

    upd 3

--- sort.py
+++ sort.py
@@ -230,8 +230,172 @@ class GnomeSort(Sort):
         return swaps, self.operations_count
 
 
-if __name__ == "__main__":
-    sort = GnomeSort(100, 50)
+class RadixLSDSort(Sort):
+    def __init__(self, tick=1000, column_count=100):
+        Sort.__init__(self, tick, column_count)
+
+    @staticmethod
+    def get_max_digit_count(array):
+        return len(str(max(array)))
+
+    @staticmethod
+    def columns_to_str_array(columns, max_digit_count):
+        return [
+            '0' * (max_digit_count - len(str(val))) + str(val) for val in columns
+            ]
+
+    def show(self):
+        self.win_init()
+        columns = self.make_columns()
+        swaps = 0
+
+        n = RadixLSDSort.get_max_digit_count(columns)
+
+        for i in range(n): # (n - i - 1) - processed digit
+            count_array = [[] for _ in range(10)]
+            array = RadixLSDSort.columns_to_str_array(columns, n)
+            
+            for curr, value in enumerate(array):
+                self.win_update(columns, curr)
+                count_array[int(value[n - i - 1])].append(value)
+            
+            array = []
+            for j in range(10):
+                array += count_array[j]
+
+            for ind, value in enumerate(array):
+                temp = columns.index(int(value))
+                self.win_update(columns, temp)
+                self.win_update(columns, ind)
+                columns[ind], columns[temp] = columns[temp], columns[ind]
+                swaps += 1
+
+        self.win_update(columns, -1)
+        while self.win.running:
+            self.win.update()
+            
+        return swaps, self.operations_count
+
+
+class RadixMSDBinSort(Sort):
+    def __init__(self, tick=1000, column_count=100):
+        Sort.__init__(self, tick, column_count)
+        self.temp_columns = []
+        self.swaps = 0
+
+    @staticmethod
+    def get_max_digit_count(array):
+        return max(len(bin(x)[2:]) for x in array)
+
+    def __msd_sort(self, array, bit_position):
+        if bit_position == 0 or len(array) < 2:
+            return array
+
+        # Split numbers based on bit at bit_position from the right
+        zeros = []
+        ones = []
+        for number in array:
+            if (number >> (bit_position - 1)) & 1:
+                ones.append(number)
+            else:
+                zeros.append(number)
+
+        # recursively split both lists further
+        zeros = self.__msd_sort(zeros, bit_position - 1)
+        ones = self.__msd_sort(ones, bit_position - 1)
+
+        # recombine lists
+        array = zeros + ones
+
+        # sorting vizualization
+        for ind, value in enumerate(array):
+            temp = self.temp_columns.index(value)
+            self.win_update(self.temp_columns, temp)
+            self.win_update(self.temp_columns, ind)
+            self.temp_columns[ind], self.temp_columns[temp] = self.temp_columns[temp], self.temp_columns[ind]
+            self.swaps += 1
+    
+        return array
+
+    def show(self):
+        self.win_init()
+        columns = self.make_columns()
+        self.temp_columns = columns
+        self.swaps = 0
+
+        n = RadixMSDBinSort.get_max_digit_count(columns)
+        columns = self.__msd_sort(columns, n)
+
+        self.win_update(columns, -1)
+        while self.win.running:
+            self.win.update()
+            
+        return self.swaps, self.operations_count
+
+
+class RadixMSDSort(Sort):
+    def __init__(self, tick=1000, column_count=100):
+        Sort.__init__(self, tick, column_count)
+        self.temp_columns = []
+        self.swaps = 0
+
+    @staticmethod
+    def get_max_digit_count(array):
+        return len(str(max(array)))
+
+    @staticmethod
+    def get_digit_from_number(number, position):
+        if len(str(number)) < position:
+            return 0
+        number = str(number)[::-1]
+        return int(number[position - 1])
+
+    def __msd_sort(self, array, position):
+        if position == 0 or len(array) < 2:
+            return array
+
+        # Split numbers based on digit at position from the right
+        digit_list = [[] for _ in range(10)]
+        for number in array:
+            digit = RadixMSDSort.get_digit_from_number(number, position)
+            digit_list[digit].append(number)
+        
+        # recursively split all lists further
+        for digit in range(10):
+            digit_list[digit] = self.__msd_sort(digit_list[digit], position - 1)
+
+        # recombine lists
+        array = []
+        for digit in range(10):
+            array += digit_list[digit]
+        
+        # sorting vizualization
+        for ind, value in enumerate(array):
+            temp = self.temp_columns.index(value)
+            self.win_update(self.temp_columns, temp)
+            self.win_update(self.temp_columns, ind)
+            self.temp_columns[ind], self.temp_columns[temp] = self.temp_columns[temp], self.temp_columns[ind]
+            self.swaps += 1
+    
+        return array
+
+    def show(self):
+        self.win_init()
+        columns = self.make_columns()
+        self.temp_columns = columns
+        self.swaps = 0
+
+        n = RadixMSDSort.get_max_digit_count(columns)
+        columns = self.__msd_sort(columns, n)
+
+        self.win_update(columns, -1)
+        while self.win.running:
+            self.win.update()
+            
+        return self.swaps, self.operations_count
+
+if __name__ == "__main__":    
+    sort = RadixMSDBinSort(300, 999)
     
     swaps, operations = sort.show()
     
